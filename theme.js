f53e122e5ef7e15bb1385e5b2ff35337c1c6bb26
Anuna29 <anusukh29@gmail.com>
2024-07-29 20:17:13 -0500

    added portfolio

--- theme.js
+++ theme.js
@@ -1,16 +1,7 @@
 const themeButton = document.getElementById("change-theme-button")
 
-function changeText(){
-  const text = document.getElementById("title")
 
-  if (theme == "light"){
-    text.innerHTML = "Dark theme"
-    theme = "dark";
-  }else {
-    text.innerHTML = "Light theme"
-    theme="light"
-  }
-}
+
 
 
 
